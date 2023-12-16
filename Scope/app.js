8802044d6761645f3280ffad27d659019ad32903
devesabbir <heartedsabbirdhrubo@gmail.com>
2023-12-16 13:59:22 +0600

    Added Scope Folder and describe var let and const keyword

---
+++ Scope/app.js
@@ -0,0 +1,21 @@
+var lang = "Bangla"
+
+function learn (topic) {
+    var lang = topic
+    console.log(`I am learning ${lang}`)
+}
+
+learn('English')
+
+console.log(lang)
+
+/**
+ * 
+ * 
+ * @var is reasignable and accessible as global variables 
+ * @let is reasignable and accessible only on block area
+ * @const is not reasignable but changeable reference values
+ * 
+ * 
+ */
+
