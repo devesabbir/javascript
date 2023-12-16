a471e58fd16f7ba0f954954fdf7f0f2b6b4ea4fd
devesabbir <heartedsabbirdhrubo@gmail.com>
2023-12-16 18:12:09 +0600

    Javascript dom event listining approach

---
+++ Event/app.js
@@ -0,0 +1,11 @@
+const btnEl = document.getElementById('btn');
+
+
+// best approach
+btnEl.addEventListener('click', function(){
+    console.log('Hello world')
+})
+
+function hello () {
+    console.log('Hello world')
+}
